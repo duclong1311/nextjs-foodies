edd24cc92e76163239bab820a98b6eb75e415b88
duclong1311 <duclong1311.work@gmail.com>
2025-02-19 23:00:13 +0700

    create route & dynamic route

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage() {
+    return (
+        <div>
+            <h1>Meal Details Page</h1>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <p><Link href="/meals"></Link></p>
+      <p><Link href="/meals/share"></Link></p>
+      <p><Link href="/community"></Link></p>
     </main>
   );
 }
